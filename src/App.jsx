835657efedccb4881f2d3937da1bec5a15b1485d
guLazzuri <165019865+guLazzuri@users.noreply.github.com>
2025-02-08 21:09:37 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import AddTasks from "./components/AddTasks";
+import Tasks from "./components/Tasks";
+import { v4 } from "uuid";
+import Title from "./components/Title";
+
+function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    async function fetchTasks() {
+      // CHAMAR A API
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "GET",
+        }
+      );
+      // PEGAR OS DADOS QUE ELA RETORNA
+      const data = await response.json();
+      //ARMAZENAR/PERSISITIR OS DADOS NO STATE
+      setTasks(data);
+    }
+    //CHAMAR A FUNÇÃO / API
+    //fetchTasks();
+  }, []);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onTaskDelete(taskid) {
+    const indice = tasks.findIndex((task) => task.id === taskid);
+    const newTasks = [...tasks];
+    newTasks.splice(indice, 1);
+    setTasks(newTasks);
+  }
+
+  function onTaskAdd(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
+  return (
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6 ">
+      <div className="w-[500px] space-y-3">
+        <Title> GERENCIADOR DE TAREFAS </Title>
+        <AddTasks onTaskAdd={onTaskAdd} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onTaskDelete={onTaskDelete}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/TaskPage.jsx
@@ -0,0 +1,39 @@
+import { ChevronLeftIcon } from "lucide-react";
+import { useSearchParams, useNavigate } from "react-router-dom";
+import Title from "../components/Title";
+
+function TaskPage() {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
+
+  ///////////////////////////////////////////////////////////
+  function backToHome() {
+    navigate("/");
+  }
+  ///////////////////////////////////////////////////////////
+  return (
+    <div className="h-screen w-screen bg-slate-500 p-6">
+      <p>{navigate}</p>
+      <div className="w-[500px] space-y-3">
+        <div className=" flex justify-center relative">
+          <button
+            onClick={backToHome}
+            className="absolute left-0 top-0 bottom-0"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <Title>DETALHES DE TAREFAS</Title>
+        </div>
+
+        <div className="bg-slate-300 p-4 rounded-md shadow">
+          <h2 className="text-xl font-bold text-slate-800">{title}</h2>
+          <p className="text-slate-700">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TaskPage;
