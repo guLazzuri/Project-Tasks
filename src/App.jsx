1d53d04628faf5c223193802908bb62171191062
guLazzuri <165019865+guLazzuri@users.noreply.github.com>
2025-02-18 11:49:16 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -61,7 +61,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6 ">
       <div className="w-[500px] space-y-3">
-        <Title> GERENCIADOR DE TAREFAS </Title>
+        <Title> TASK WEB </Title>
         <AddTasks onTaskAdd={onTaskAdd} />
         <Tasks
           tasks={tasks}
